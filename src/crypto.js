0b4d199cd926f9787f20416f303eb550b0eb7dde
JS <info@psygo.ch>
2019-04-11 16:47:26 +0200

    Fix the formatBytes function

--- src/crypto.js
+++ src/crypto.js
@@ -161,9 +161,19 @@
         $('#new-button').css('display', encrypted ? 'inline-block' : 'none');
     }
 
-    function b2Mb(fileLength) {
-        return Number(fileLength / (1024 * 1024)).toFixed(0) + 'Mb';
+    function b2Mb(bytes, decimals = 2) {
+        if (bytes === 0) return '0 Bytes';
+
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
     }
+
+
     var separator = "||",
         password = undefined,
         errorMsg1 = "Error: Wrong encryption password !",
